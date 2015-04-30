92e114179453ca2697d9d7f8ae090cffe79d00ef
GWEN <gwenmak@gmail.com>
2015-04-29 23:22:44 -0400

    w07d03 hw completed

--- timers.js
+++ timers.js
@@ -3,6 +3,7 @@ var start = document.querySelector("#start");
 var pause = document.querySelector("#pause");
 var timer = document.querySelector("#timer");
 var counter = 0;
+var itIsWorking = 0;
 
 var startTimer = function(){
 	timer.innerHTML = "Time elapsed: " + counter;
@@ -10,26 +11,20 @@ var startTimer = function(){
 }
 
 start.addEventListener("click", function(){
-	if (timer.innerHTML === "Stop Watch"){
-		startTimer();
-		setInterval(startTimer, 1000)
+	if (itIsWorking === 0){
+		interval = setInterval(startTimer, 1000);
+		itIsWorking = 1
 	}
 })
 
-// start.addEventListener("click", function(){
-// 	if (timer.innerHTML === "Stop Watch"){
-// 		setInterval(function(){
-// 			timer.innerHTML = "Time elapsed: " + counter; 
-// 			counter++;
-// 		}, 1000)
-// 	}
-// })
-
-pause.addEventListener("click", function(event){
-
+pause.addEventListener("click", function(){
+	clearInterval(interval);
+	itIsWorking = 0
 })
 
-reset.addEventListener("click", function(event){
+reset.addEventListener("click", function(){
+	clearInterval(interval);
 	counter = 0;
-	timer.innerHTML = "Time elapsed: " + counter
+	timer.innerHTML = "Stop Watch";
+	itIsWorking = 0
 })
